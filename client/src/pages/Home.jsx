bfba689b2b3b5ac29c6c59fa98c9999a6584f854
Aryan Tiwari <taryan54@gmail.com>
2026-07-05 15:50:31 +0530

    PHASE-0: Update home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import Hero from "../sections/Hero"
+import About from "../sections/About"
+import Experience from "../sections/Experience"
+import Skills from "../sections/Skills"
+import FeaturedProjects from "../sections/FeaturedProjects"
+import ContactSection from "../sections/ContactSection"
+
 function Home() {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+        <Hero />
+        <About />
+        <Experience />
+        <Skills />
+        <FeaturedProjects />
+        <ContactSection />
+    </>
   );
 }
 
